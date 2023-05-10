480cf6eafcffe16a90f1de84cf6bca803e34321a
Manaia Junior <manaiajr.23@gmail.com>
2023-05-10 03:29:20 -0300

    Update

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
 
 export default function Page() {
-  return <h1>Blog! <Link href="/dashboard/">Dashboard</Link> | <Link href="/">Home</Link></h1>
+  return <h1>Blog <Link href="/dashboard/">Dashboard</Link> | <Link href="/">Home</Link></h1>
 }
